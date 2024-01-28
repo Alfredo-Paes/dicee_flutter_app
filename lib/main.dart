44bb2f16ed1ac2637eb033292061117e832ab3a5
Alfredo Paes da Luz <alfredo.alfpaes@gmail.com>
2024-01-28 17:11:10 -0300

    Logic to handler dice numbers was apply

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,24 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void handlerDiceNumber () {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset(
-                    'images/dice1.png'),
-              )), //Expanded is a widget to apply some like responsiveness on screen
+            child: TextButton(
+              onPressed: () {
+                handlerDiceNumber();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ), //Expanded is a widget to apply some like responsiveness on screen
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset(
-                    'images/dice1.png'),
-              ))
+            child: TextButton(
+              onPressed: () {
+                handlerDiceNumber();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
         ],
       ),
     );
